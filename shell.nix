86a5ab4c76bbdd59704927286ca7827bd97d143b
Bauke <me@bauke.xyz>
2024-01-22 17:53:04 +0100

    Prepare to port the project over to Rust.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+let
+  rustup-toolchain = rust-bin.fromRustupToolchainFile ./rustup-toolchain.toml;
+in
+mkShell rec {
+  packages = [
+    cargo-audit
+    cargo-edit
+    cargo-insta
+    cargo-make
+    cargo-outdated
+    gegl.dev
+    imagemagick
+    rustup-toolchain
+  ];
+}
